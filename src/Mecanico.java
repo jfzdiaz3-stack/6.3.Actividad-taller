6bad295fef45a9ec6c8f25cd4d530c10abdcc975
jfzdiaz3-stack <jfzdiaz.3@gmail.com>
2026-02-25 23:48:44 +0100

    Agregar clase Mecanico implementando Especialista

---
+++ src/Mecanico.java
@@ -0,0 +1,12 @@
+public class Mecanico implements Especialista {
+    private String nombre;
+
+    public Mecanico(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public void reparar(Reparacion r) {
+        // No hace falta implementar nada
+    }
+}

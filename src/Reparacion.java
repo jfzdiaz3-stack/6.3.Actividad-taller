d219c1e5a4410ef2e2eacca991da4f9231a11d6d
jfzdiaz3-stack <jfzdiaz.3@gmail.com>
2026-02-25 23:47:37 +0100

    Crear clase Reparacion con descripción y fecha

---
+++ src/Reparacion.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Reparacion {
+    private String descripcion;
+    private Date fecha;
+
+    public Reparacion(String descripcion, Date fecha) {
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+    }
+}

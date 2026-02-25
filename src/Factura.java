be2a29947b62e7cfdce3b4c4e7547fb51f434ce9
jfzdiaz3-stack <jfzdiaz.3@gmail.com>
2026-02-26 00:09:11 +0100

    Agregar clase Factura con constructor

---
+++ src/Factura.java
@@ -0,0 +1,9 @@
+public class Factura {
+    private int numero;
+    private double importe;
+
+    public Factura(int numero, double importe) {
+        this.numero = numero;
+        this.importe = importe;
+    }
+}

590607334b0ccad4023b1f36bba4e4f0e5ae6ced
jfzdiaz3-stack <jfzdiaz.3@gmail.com>
2026-02-25 23:42:40 +0100

    Añadir clase abstracta Vehiculo con propiedades y constructor

---
+++ src/Vehiculo.java
@@ -0,0 +1,9 @@
+public abstract class Vehiculo {
+    private String matricula;
+    private String modelo;
+
+    public Vehiculo(String matricula, String modelo) {
+        this.matricula = matricula;
+        this.modelo = modelo;
+    }
+}
